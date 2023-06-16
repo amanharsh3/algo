bd6a173a0e44c9c5b91aaf0d84f6c5b26d65c746
amanharsh3 <97787149+amanharsh3@users.noreply.github.com>
2023-06-16 17:51:23 +0530

    Create union_find_size.cc

---
+++ union_find/union_find_size.cc
@@ -0,0 +1,64 @@
+#include <iostream>
+#include <numeric>
+#include <vector>
+using namespace std;
+
+struct union_find {
+    vector<int> parent;
+    vector<int> size;
+    int components = 0;
+
+    union_find(int n = -1) {
+        if (n >= 0)
+            init(n);
+    }
+
+    void init(int n) {
+        parent.resize(n + 1);
+        iota(parent.begin(), parent.end(), 0);
+        size.assign(n + 1, 1);
+        components = n;
+    }
+
+    int find(int x) {
+        return x == parent[x] ? x : parent[x] = find(parent[x]);
+    }
+
+    bool unite(int x, int y) {
+        x = find(x);
+        y = find(y);
+
+        if (x == y)
+            return false;
+
+        if (size[x] < size[y])
+            swap(x, y);
+
+        parent[y] = x;
+        size[x] += size[y];
+        components--;
+        return true;
+    }
+};
+
+
+int main() {
+    ios::sync_with_stdio(false);
+#ifndef NEAL_DEBUG
+    cin.tie(nullptr);
+#endif
+
+    int N, Q;
+    cin >> N >> Q;
+    union_find UF(N);
+
+    for (int q = 0; q < Q; q++) {
+        int type, a, b;
+        cin >> type >> a >> b;
+
+        if (type == 1)
+            cout << (UF.find(a) == UF.find(b)) << '\n';
+        else if (type == 2)
+            cout << UF.unite(a, b) << '\n';
+    }
+}
